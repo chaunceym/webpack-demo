fafaa833b68156b22235b7c90b72a4ab3c62b899
Chauncey <18322276773@163.com>
2019-12-22 10:09:58 +0800

    good

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './x.less'
 import './z.styl'
+import './assets/pink.jpg'
 import x from './x.js'
 console.log(x)
+
+const div = document.querySelector('.cla')
+div.innerHTML = `<img src='./assets/pink.jpg'>`
