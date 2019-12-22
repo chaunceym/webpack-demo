e5aa80cfff9865e097ffc024758fae09b64cf716
Chauncey <18322276773@163.com>
2019-12-22 10:46:59 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import './x.less'
 import './z.styl'
-import './assets/pink.jpg'
+import jpg from './assets/pink.jpg'
 import x from './x.js'
 console.log(x)
 
 const div = document.querySelector('.cla')
-div.innerHTML = `<img src='./assets/pink.jpg'>`
+div.innerHTML = `<img src='${jpg}'>`
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = ()=>{
+    const promise = import('./lazy') // 异步加载会生成一个promise对象
+    promise.then((module)=>{
+        module.default()
+    },()=>{
+        console.log('模块加载错误')
+    })
+}
+
+
+div.appendChild(button)
